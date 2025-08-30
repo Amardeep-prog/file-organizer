c2979b7ac43bb4616a89ee057aaada1bb97499a9
Amardeep-prog <deepuamardeep7@gmail.com>
2025-08-30 23:42:12 +0530

    First project upload

---
+++ React-Inventory-Management-System-main/demo.js
@@ -0,0 +1,12 @@
+const nums=[1,2,3,4,5];
+console.log(...nums);
+
+
+const arr1=[1,2,3];
+const arr2=[...arr1];
+console.log(arr2);
+
+
+const even=[2,4,6];
+const odd=[3,5,7,9];
+console.log(10,20,...even,...odd);
